53e8c6cca145cd8f6641fffaef404a9f1ddddbcc
Gonzalo Diaz <egonzalodm@gmail.com>
2020-09-23 17:37:32 -0500

    Frontend montado en app engine

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <Usuario nombre="Gonzalo Diaz"></Usuario>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Usuario from './components/Usuario.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Usuario
   }
 }
 </script>
---
+++ src/components/Usuario.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="user">
+      <h1>Landing page WHEELS US</h1>
+      <h2>Coming soon!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Usuario",
+    props:{
+        nombre: String
+    },
+    data(){
+        return{
+        telefono:0
+    }}
+
+}
+</script>
+
+<style>
+
+</style>
