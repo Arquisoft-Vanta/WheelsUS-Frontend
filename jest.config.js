101d0a7e118029815814759f44c57604021e49fa
Sebastian Reina <semorenore@unal.edu.co>
2020-09-24 15:39:31 -0500

    Revert "Adicion de Jest"
    This reverts commit 1ffce75ac198760bea43ef7142341e2f45ac186f.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,vue}", "!src/main.js"],
-};
