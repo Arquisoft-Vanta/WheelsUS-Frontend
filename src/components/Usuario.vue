2d789d03d27b83b8b4eebb9606fcd0d8234faa07
Gonzalo Diaz <egonzalodm@gmail.com>
2020-09-23 22:33:12 -0500

    Feature#66 crear y montar frontend en gcp (#1)
    * Frontend montado en app engine
    
    * Bootstrap funcionando

---
+++ src/components/Usuario.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="user">
+      <h1>Landing page WHEELS US</h1>
+      <h2>Coming soon!</h2>
+      <p>Hola mundo</p>
+      <p>{{nombre}}</p>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Usuario",
+    props:{
+        nombre: String
+    },
+    data(){
+        return{
+        telefono:0
+    }}
+
+}
+</script>
+
+<style>
+
+</style>
