b8ec1a333a14b155934255c133bc72b85a1eb0ba
Sebastian Reina <semorenore@unal.edu.co>
2020-09-24 13:10:12 -0500

    Se empieza la creación del fromulario

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Signup> 
+  </Signup>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Signup from './components/signup.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Signup
   }
 }
 </script>
