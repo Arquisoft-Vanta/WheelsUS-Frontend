311752b6977ce660ed2d63c5aa8abe76aa4d1924
Francisco Dueñas <frsduenasca@unal.edu.co>
2020-10-03 20:55:00 -0500

    Archivo "jest.config.js" se modifico

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  preset: '@vue/cli-plugin-unit-jest',
   transform: {
-    "^.+\\.vue$": "vue-jest",
+    '^.+\\.vue$': 'vue-jest',
   },
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,vue}", "!src/main.js"],
