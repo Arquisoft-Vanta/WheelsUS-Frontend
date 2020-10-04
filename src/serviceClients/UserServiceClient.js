f0dc0f7265953d23881e9df415bad2e3149b953a
Gonzalo Diaz <egonzalodm@gmail.com>
2020-10-03 23:30:42 -0500

    Clientes de servicio de user y vehicle

---
+++ src/serviceClients/UserServiceClient.js
@@ -0,0 +1,64 @@
+const axios = require("axios");
+const route = "http://localhost:8080/api/user/";
+function getUsers(callback) {
+  axios
+    .get(route)
+    .then(response => {
+      callback(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function getUser(id, callback) {
+  axios
+    .get(`${route}${id}`)
+    .then(response => {
+      callback(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function createUser(user, callback) {
+  axios
+    .post(route, user)
+    .then(response => {
+      callback(response.status);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function updateUser(user, callback) {
+  axios
+    .put(route+"0", user)
+    .then(response => {
+      callback(response.status);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function deleteUser(id, callback) {
+  axios
+    .delete(`${route}${id}`)
+    .then(response => {
+      callback(response.status);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+export default {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+};
