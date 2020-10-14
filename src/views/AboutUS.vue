42c2032e7f1a9e91312ebde167e008ab73852f47
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-10-14 17:42:36 -0500

    Se agrego el componente AboutUS a la carpeta views

---
+++ src/views/AboutUS.vue
@@ -0,0 +1,52 @@
+<template>
+  <Header></Header>
+  <div id="aboutus">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 justify-content-center">
+          <div class="informacion card">
+            <div class="card-body">
+              <h5 class="card-title">Acerca de nosotros</h5>
+              <p class="card-text pt-4 pb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                posuere erat a ante.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <FooterwithBackground></FooterwithBackground>
+</template>
+
+<script>
+import Header from "../components/Header";
+import FooterwithBackground from "../components/FooterwithBackground.vue";
+
+export default {
+  name: "Usuario",
+  components: {
+    Header,
+    FooterwithBackground,
+  },
+  data() {
+    return {
+      informacion: "Información",
+    };
+  },
+};
+</script>
+
+<style>
+.informacion {
+  opacity: 95%;
+}
+
+#aboutus {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 85vh;
+}
+</style>
