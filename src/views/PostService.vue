0ed740e91e4b817f2a0a59476cdbb7127b916a44
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-10-22 03:04:04 -0500

    Modificación del margen de los componentes

--- src/views/PostService.vue
+++ src/views/PostService.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <Header></Header>    
+    <Header></Header>
     <div>
       <div class="container">
         <div class="row justify-content-between">
           <div class="col2 col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4 mt-4">
             <div class="createservice card">
               <div class="card-body">
-                <OriginDestination />                
+                <OriginDestination />
               </div>
             </div>
           </div>
-          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-8 mt-8">
+          <div
+            class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-8 mt-0 mt-md-8 mb-5 mb-md-0"
+          >
             <DirectionsMapView />
           </div>
         </div>
@@ -33,9 +35,7 @@ import firebase from "firebase";
 export default {
   name: "PostService",
   data() {
-    return {
-    
-    };
+    return {};
   },
   components: {
     OriginDestination,
@@ -43,8 +43,7 @@ export default {
     Header,
     FooterwithBackground,
   },
-  methods: {
-  },
+  methods: {},
   mounted() {
     EventBus.$emit("passengerRoutes-data", this.routes);
   },
@@ -57,11 +56,11 @@ export default {
   background-color: white;
   opacity: 90%;
   border-radius: 2%;
-  margin: 10% 0 20% 0;
+  /* margin: 10% 0 20% 0; */
 }
 .mapg {
   opacity: 100%;
   border-radius: 2%;
-  margin: 5% 0 0% 0;
+  margin: 0% 0 6% 5%;
 }
 </style>
