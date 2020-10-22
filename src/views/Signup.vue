ad81d3c79594588da0eee1503b2bc5159361e19d
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-10-22 03:03:33 -0500

    Modificación del logo, colores y espaciado

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,165 +1,175 @@
 <template>
-<div>
-  <div class="fixed-top">
-    <nav id="Banner" class="navbar navbar-expand-lg">
-      <div class="col-auto">
-        <a id="Titulo" class="navbar-brand text-white" href="#">
-          <img
-            src="~@/assets/carrito.png"
-            width="40"
-            height="40"
-            alt=""
-            loading="lazy"
-          />
-          Wheels US
-        </a>
-      </div>
-      <button
-        class="navbar-toggler navbar-light bg-light"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto"></ul>
-        <ul class="navbar-nav mr-right">
-          <li class="nav-item active">
-            <router-link to="about-us" class="nav-link text-white" href="#"
-                  >Acerca de nosotros<span class="sr-only"></span
-                ></router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/signup" class="nav-link text-white"
-              >Ingresar</router-link
-            >
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div
-        class="col align-self-center col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5 mt-5"
-      >
-        <div class="signup card">
-          <div class="form-col align-items-center">
-            <div class="card-body">
-              <div class="col-auto">
-                <img
-                  class="logo"
-                  alt="logo"
-                  src="~@/assets/logo.png"
-                  style="margin: 0 0 5% 0"
-                />
-                <div class="form-group">
-                  <label for="name"></label>
-                  <input
-                    v-model="name"
-                    type="text"
-                    class="form-control"
-                    placeholder="Nombre completo"
-                    name="name"
-                    id="inputName"
-                    required
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="phone"></label>
-                  <input
-                    v-model="phone"
-                    type="tel"
-                    class="form-control"
-                    placeholder="Telefono"
-                    name="phone"
-                    pattern="[0-9]{10}"
-                    id="inputPhone"
-                    required
-                  />
-                </div>
+  <div>
+    <div class="fixed-top">
+      <nav id="Banner" class="navbar navbar-expand-lg">
+        <div class="col-auto">
+          <a id="Titulo" class="navbar-brand text-white" href="#">
+            <img
+              src="~@/assets/logo.png"
+              width="40"
+              height="40"
+              alt=""
+              loading="lazy"
+            />
+            Wheels US
+          </a>
+        </div>
+        <button
+          class="navbar-toggler navbar-light bg-light"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto"></ul>
+          <ul class="navbar-nav mr-right">
+            <li class="nav-item active">
+              <router-link to="about-us" class="nav-link text-white" href="#"
+                >Acerca de nosotros<span class="sr-only"></span
+              ></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link text-white"
+                >Ingresar</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div
+          class="col align-self-center col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5 mt-5"
+        >
+          <div class="signup card">
+            <div class="form-col align-items-center">
+              <div class="card-body">
                 <div class="col-auto">
+                  <img
+                    class="logo"
+                    alt="logo"
+                    src="~@/assets/logo.png"
+                    style="margin: 0 0 5% 0"
+                  />
+                  <h5>Ingresa tus datos</h5>
+                  <div class="form-group">
+                    <label for="name"></label>
+                    <input
+                      v-model="name"
+                      type="text"
+                      class="form-control"
+                      placeholder="Nombre completo"
+                      name="name"
+                      id="inputName"
+                      required
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="phone"></label>
+                    <input
+                      v-model="phone"
+                      type="tel"
+                      class="form-control"
+                      placeholder="Telefono"
+                      name="phone"
+                      pattern="[0-9]{10}"
+                      id="inputPhone"
+                      required
+                    />
+                  </div>
+                  <div class="col-auto">
+                    <div class="form-row">
+                      <div class="col">
+                        <input
+                          v-model="user"
+                          style="width: 100%; margin: 0 -7% 5% -4%"
+                          type="text"
+                          class="form-control"
+                          placeholder="Usuario"
+                          name="user"
+                          id="inputUser"
+                          required
+                        />
+                      </div>
+                      <div class="col">
+                        <label for="staticEmail" style="margin: 5% 0 0 0">
+                          @unal.edu.co
+                        </label>
+                      </div>
+                    </div>
+                  </div>
                   <div class="form-row">
                     <div class="col">
+                      <label for="psw"></label>
                       <input
-                        v-model="user"
-                        style="width: 100%; margin: 0 -7% 5% -4%"
-                        type="text"
+                        style="width: 93%; margin: 0 -12% 5% -4%;"
+                        v-model="psw"
+                        type="password"
                         class="form-control"
-                        placeholder="Usuario"
-                        name="user"
-                        id="inputUser"
+                        placeholder="Contraseña"
+                        name="psw"
+                        id="inputPsw"
                         required
                       />
                     </div>
                     <div class="col">
-                      <label for="staticEmail" style="margin: 5% 0 0 0">
-                        @unal.edu.co
-                      </label>
+                      <label for="pswRepeat"></label>
+                      <input
+                        style="width: 93%; margin: 0 -4% 5% -12%"
+                        v-model="pswrepeat"
+                        type="password"
+                        class="form-control"
+                        placeholder="Confirmar"
+                        name="pswRepeat"
+                        id="inputpswRepeat"
+                        required
+                      />
                     </div>
                   </div>
-                </div>
-                <div class="form-row">
-                  <div class="col">
-                    <label for="psw"></label>
-                    <input
-                      style="width: 93%; margin: 0 -12% 5% -4%;"
-                      v-model="psw"
-                      type="password"
-                      class="form-control"
-                      placeholder="Contraseña"
-                      name="psw"
-                      id="inputPsw"
-                      required
-                    />
+                  <p>
+                    Al crear una cuenta estas de acuerdo con nuestros
+                    <a href="#" style="color: #06416d">Terminos y Condiciones</a
+                    >.
+                  </p>
+                  <div class="col-auto" position="center">
+                    <a
+                      href="#"
+                      type="button"
+                      class="btn btn-outline-danger"
+                      name="cancelBtn"
+                      id="cancelBtn"
+                      style="margin: -1% 2% 0 2%"
+                    >
+                      Cancelar
+                    </a>
+                    <button
+                      class="btn btn-outline-dark"
+                      id="submitBtn"
+                      style="margin: 0% 2% 0 2%"
+                      @click="goTo"
+                    >
+                      Registrarse
+                    </button>
                   </div>
-                  <div class="col">
-                    <label for="pswRepeat"></label>
-                    <input
-                      style="width: 93%; margin: 0 -4% 5% -12%"
-                      v-model="pswrepeat"
-                      type="password"
-                      class="form-control"
-                      placeholder="Confirmar"
-                      name="pswRepeat"
-                      id="inputpswRepeat"
-                      required
-                    />
+                  <div style="margin: 2% 0% 0% 0%" position="center">
+                    <button
+                      @click="goTo"
+                      class="btn btn-outline-dark btn-block"
+                    >
+                      <img
+                        class="logo mr-2"
+                        src="~@/assets/google.png"
+                        width="20"
+                      />
+                      Regístrate con Google
+                    </button>
                   </div>
-                </div>
-                <p>
-                  Al crear una cuenta estas de acuerdo con nuestros
-                  <a href="#" style="color: #06416d">Terminos y Condiciones</a>.
-                </p>
-                <div class="col-auto" position="center">
-                  <a
-                    href="#"
-                    type="button"
-                    class="btn btn-light"
-                    name="cancelBtn"
-                    id="cancelBtn"
-                    style="margin: -1% 2% 0 2%"
-                  >
-                    Cancelar
-                  </a>
-                  <button
-                    class="btn btn-primary"
-                    id="submitBtn"
-                    style="margin: -1% 2% 0 2%"
-                    @click="goTo"
-                  >
-                    Registrarse
-                  </button>
-                </div>
-                <div style="margin: 2% 5% 5% 5%" position="center">
-                  <button @click="goTo" class="btn btn-primary">
-                    <img class="logo" src="~@/assets/google.png" width="30" />
-                    Regístrate con Google
-                  </button>
                 </div>
               </div>
             </div>
@@ -167,8 +177,7 @@
         </div>
       </div>
     </div>
-  </div>
-  <FooterwithBackground></FooterwithBackground>
+    <FooterwithBackground></FooterwithBackground>
   </div>
 </template>
 
