c47d0539d9eccd68048d6eb12eb66f73b6bcc31e
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-11-05 12:00:03 -0500

    Se agregó al index.js PassangerProfile

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import PostService from "./../views/PostService.vue";
 import Passenger from "./../views/Passenger.vue";
 import Driver from "./../views/Driver.vue";
 import RideRegistration from "./../views/RideRegistration.vue";
+import PassengerProfile from "./../views/PassangerProfile.vue";
 
 Vue.use(VueRouter);
 
@@ -36,6 +37,11 @@ const routes = [
   { path: "/create-service", name: "createService", component: CreateService },
   { path: "/passenger", name: "passenger", component: Passenger },
   { path: "/driver", name: "driver", component: Driver },
+  {
+    path: "/passanger-profile",
+    name: "passanger-profile",
+    component: PassengerProfile,
+  },
 ];
 
 const router = new VueRouter({
