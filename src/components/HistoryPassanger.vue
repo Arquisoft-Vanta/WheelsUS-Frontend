6f8cf86a22d90c34d109d075153db466ade780ce
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-11-09 10:19:18 -0500

    Se agregó el componente HistoryPassanger.vue

---
+++ src/components/HistoryPassanger.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="history container overflow-auto bg-white">
+    <div class="list-group h-100">
+      <div class="accordion" id="accordionExample">
+        <div
+          class="card border shadow-sm mt-2 mb-2 bg-white rounded"
+          v-for="route in routes"
+          :key="route.id"
+        >
+          <p class="mb-0">
+            <button
+              class="btn btn-link btn-block text-left"
+              type="button"
+              :data-target="`#data${route.id}`"
+              aria-expanded="true"
+              :aria-controls="`data${route.id}`"
+              style="color: #06416d"
+              @click="routePassengerItemPressed(route)"
+            >
+              {{ route.origin.address }} - {{ route.destination.address }}
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import { EventBus } from "@/EventBus.js";
+
+export default {
+  components: {},
+  data() {
+    return {
+      routes: [],
+      routeSelected: [],
+      selected: "",
+    };
+  },
+  created() {
+    const db = firebase.firestore();
+    db.collection("passengerRoutes").onSnapshot((snap) => {
+      this.routes = [];
+      snap.forEach((doc) => {
+        let route = doc.data();
+        route.id = doc.id;
+        this.routes.push(route);
+      });
+    });
+  },
+  methods: {
+    routePassengerItemPressed(route) {
+      EventBus.$emit("passengerRoutes-data", [route]);
+    },
+  },
+};
+</script>
+
+<style></style>
