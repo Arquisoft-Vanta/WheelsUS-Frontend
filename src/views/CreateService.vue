b934aad5fded223f19aee85f86ce36171a0c159c
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-11-09 15:04:55 -0500

    Se removió un comentario

--- src/views/CreateService.vue
+++ src/views/CreateService.vue
@@ -196,7 +196,6 @@
                     </button>
                   </div>
                 </form>
-                <!--<OriginDestination />-->
               </div>
             </div>
           </div>
