c465f42f67a67724796fc229a7d133d3b73ae144
Francisco Dueñas <43359099+LeronArenwino@users.noreply.github.com>
2020-12-10 23:30:03 -0500

    Nuevo diseño de las cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,51 @@
 <template>
   <div id="home">
     <Header></Header>
-    <div class="container-fluid">
+    <div class="container-fluid mb-5">
       <div class="row">
-        <div class="col-12 col-md-6 mt-0 mb-0 mt-md-5 mb-md-0">
+        <div class="col-12 col-md-6 mt-4 mb-5">
           <div
-            class="card border shadow mt-2 mb-2 bg-white rounded text-center mr-0 ml-0 mr-sm-5 ml-sm-5 mt-xs-0 mb-xs-0 mt-5"
+            class="card bg-dark text-white border shadow rounded text-center mr-0 ml-0 mr-sm-5 ml-sm-5"
+            type="button"
+            @click="goToDrive"
           >
+            <img
+              class="card-img-top"
+              src="~@/assets/driver.jpg"
+              alt="Card image cap"
+            />
             <div class="card-body">
-              <h1 class="card-title pt-3">Conductor</h1>
-              <p class="card-text font-weight-normal pt-2">
-                Ingresa en esta sección y descubre todo lo que la plataforma
-                tiene para ti como <strong>conductor</strong>.
+              <h4 class="card-title d-none d-md-block mb-0">Conductor</h4>
+              <h5 class="card-title d-block d-md-none mb-0">Conductor</h5>
+              <p class="card-text d-none d-md-block mb-0">
+                <small>
+                  Descubre todo lo que la plataforma tiene para ti como
+                  <strong>conductor</strong>.</small
+                >
               </p>
-              <a href="" class="btn btn-dark btn-block btn-lg mt-4" @click="goToDrive"
-                >Entrar</a
-              >
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-6 mt-0 mb-5 mt-md-5 mb-md-0">
+        <div class="col-12 col-md-6 mt-4 mb-5">
           <div
-            class="card text-dark bg-white rounded shadow border shadow mt-2 mb-2 text-center mr-0 ml-0 mr-sm-5 ml-sm-5 mt-xs-0 mb-xs-0 mt-5 mb-5"
+            class="card bg-dark text-white rounded shadow border shadow text-center mr-0 ml-0 mr-sm-5 ml-sm-5"
+            type="button"
+            @click="goToPassenger"
           >
+            <img
+              class="card-img-top"
+              src="~@/assets/passenger.jpg"
+              alt="Card image cap"
+            />
             <div class="card-body">
-              <h1 class="card-title pt-3">Pasajero</h1>
-              <p class="card-text font-weight-normal pt-2">
-                Ingresa en esta sección y descubre todo lo que la plataforma
-                tiene para ti como <strong>pasajero</strong>.
+              <h4 class="card-title d-none d-md-block mb-0">Pasajero</h4>
+              <h5 class="card-title d-block d-md-none mb-0">Pasajero</h5>
+              <p class="card-text d-none d-md-block mb-0">
+                <small
+                  >Descubre todo lo que la plataforma tiene para ti como
+                  <strong>pasajero</strong>.</small
+                >
               </p>
-              <a href="" class="btn btn-dark btn-block btn-lg mt-4" @click="goToPassenger"
-                >Entrar</a
-              >
             </div>
           </div>
         </div>
@@ -73,4 +87,3 @@ export default {
   },
 };
 </script>
-
